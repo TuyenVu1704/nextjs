ae8e728ccf40940d8d752d06ce412ca1c0329384
Tuyevu1704 <tuyenvu1704@gmail.com>
2023-10-12 10:14:12 +0700

    list user

--- src/components/users/users.table.tsx
+++ src/components/users/users.table.tsx
@@ -1,30 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "../../styles/users.css";
 
+interface IUser {
+    email: string;
+    name: string;
+    role: string;
+}
 const UsersTable = () => {
+    const [listUser, setListUser] = useState([]);
+
     const getData = async () => {
-        const res = await fetch("http://localhost:8000/api/v1/auth/login", {
-            method: "POST",
-            body: JSON.stringify({
-                username: "hoidanit@gmail.com",
-                password: "123456",
-            }),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
         const access_token =
             "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjUyM2U0NTA5M2M4YzcyMDBmNTlmNWMyIiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIGFkbWluIiwidHlwZSI6IlNZU1RFTSIsInJvbGUiOiJBRE1JTiIsImdlbmRlciI6Ik1BTEUiLCJhZ2UiOjY5LCJpYXQiOjE2OTY5MTc2OTcsImV4cCI6MTc4MzMxNzY5N30.pqMysbKeYliicG_GFFABuEFPDgV8fzR60XwyLAFU1sU";
 
-        const data = await res.json();
-        const res1 = await fetch("http://localhost:8000/api/v1/users/all", {
+        const res = await fetch("http://localhost:8000/api/v1/users/all", {
             headers: {
                 "Content-Type": "application/json",
                 Authorization: `Bearer ${access_token}`,
             },
         });
-        const data1 = await res1.json();
-        console.log(data1);
+        const d = await res.json();
+        console.log(">>> check data fetch", d.data.result);
+        setListUser(d.data.result);
     };
 
     useEffect(() => {
@@ -36,41 +33,24 @@ const UsersTable = () => {
             <h2>User List </h2>
 
             <table>
-                <tr>
-                    <th>Company</th>
-                    <th>Contact</th>
-                    <th>Country</th>
-                </tr>
-                <tr>
-                    <td>Alfreds Futterkiste</td>
-                    <td>Maria Anders</td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td>Centro comercial Moctezuma</td>
-                    <td>Francisco Chang</td>
-                    <td>Mexico</td>
-                </tr>
-                <tr>
-                    <td>Ernst Handel</td>
-                    <td>Roland Mendel</td>
-                    <td>Austria</td>
-                </tr>
-                <tr>
-                    <td>Island Trading</td>
-                    <td>Helen Bennett</td>
-                    <td>UK</td>
-                </tr>
-                <tr>
-                    <td>Laughing Bacchus Winecellars</td>
-                    <td>Yoshi Tannamuri</td>
-                    <td>Canada</td>
-                </tr>
-                <tr>
-                    <td>Magazzini Alimentari Riuniti</td>
-                    <td>Giovanni Rovelli</td>
-                    <td>Italy</td>
-                </tr>
+                <thead>
+                    <tr>
+                        <td>Email</td>
+                        <td>Name</td>
+                        <td>Role</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listUser.map((item: IUser, index) => {
+                        return (
+                            <tr key={index}>
+                                <td>{item.email}</td>
+                                <td>{item.name}</td>
+                                <td>{item.role}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
             </table>
         </div>
     );
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-<<<<<<< HEAD
 import UsersPage from "./screens/users.page.tsx";
 // import './index.css'
 
-=======
-import UsersPage from "./screens/user.page.tsx";
-// import './index.css'
->>>>>>> 1ce3ab73136953592d42c8e2f6dfed65baa68fdb
 const router = createBrowserRouter([
     {
         path: "/",
@@ -19,16 +14,8 @@ const router = createBrowserRouter([
         path: "/users",
         element: <UsersPage />,
     },
-<<<<<<< HEAD
 ]);
 
-=======
-    {
-        path: "/tracks",
-        element: <div>manage tracks</div>,
-    },
-]);
->>>>>>> 1ce3ab73136953592d42c8e2f6dfed65baa68fdb
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         {/* <App /> */}
