3a601c0e1f6521ec911071775e4bb5bc2f1d7ef9
Tuyevu1704 <tuyenvu1704@gmail.com>
2023-10-13 12:58:47 +0700

    antd

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,72 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+    createBrowserRouter,
+    Link,
+    Outlet,
+    RouterProvider,
+} from "react-router-dom";
 import UsersPage from "./screens/users.page.tsx";
+import { useState } from "react";
+import { UserOutlined, HomeOutlined } from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Menu } from "antd";
 // import './index.css'
+
+const items: MenuProps["items"] = [
+    {
+        label: <Link to={"/"}>Home</Link>,
+        key: "home",
+        icon: <HomeOutlined />,
+    },
+    {
+        label: <Link to={"users"}>Manage Users</Link>,
+        key: "users",
+        icon: <UserOutlined />,
+    },
+];
+
+const Header: React.FC = () => {
+    const [current, setCurrent] = useState("home");
+
+    const onClick: MenuProps["onClick"] = (e) => {
+        setCurrent(e.key);
+    };
+
+    return (
+        <Menu
+            onClick={onClick}
+            selectedKeys={[current]}
+            mode="horizontal"
+            items={items}
+        />
+    );
+};
+
+const LayoutAdmin = () => {
+    return (
+        <div>
+            <Header />
+            <Outlet />
+        </div>
+    );
+};
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <App />,
-    },
-    {
-        path: "/users",
-        element: <UsersPage />,
+        element: <LayoutAdmin />,
+        children: [
+            {
+                index: true,
+                element: <App />,
+            },
+            {
+                path: "users",
+                element: <UsersPage />,
+            },
+        ],
     },
 ]);
 
